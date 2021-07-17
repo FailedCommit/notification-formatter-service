c44cacee9f75a404fe4c0a7bf0151db287e5b490
Mohit Kumar <mkumar@tekion.com>
2021-07-17 11:42:29 +0530

    Adds SMS template changes and refactoring

--- src/main/java/com/manning/notification/formatter/services/NotificationFormatterService.java
+++ src/main/java/com/manning/notification/formatter/services/NotificationFormatterService.java
@@ -4,6 +4,7 @@ import com.manning.notification.formatter.models.NotificationTemplateRequest;
 import com.manning.notification.formatter.models.NotificationTemplateRequest.NotificationParameter;
 import com.manning.notification.formatter.models.NotificationTemplateResponse;
 import lombok.AllArgsConstructor;
+import org.apache.commons.text.StringSubstitutor;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
@@ -17,35 +18,75 @@ import java.util.stream.Collectors;
 public class NotificationFormatterService {
     private final SpringTemplateEngine templateEngine;
 
-    public NotificationTemplateResponse format(NotificationTemplateRequest request) {
-        return null;
+    public NotificationTemplateResponse merge(NotificationTemplateRequest request) {
+        if ("EMAIL".equalsIgnoreCase(request.getNotificationMode())) {
+            return prepareEmailTemplate(request);
+        } else if ("SMS".equalsIgnoreCase(request.getNotificationMode())) {
+            return prepareSMSTemplate(request);
+        }
+
+        return new NotificationTemplateResponse();
     }
 
-    public NotificationTemplateResponse merge(NotificationTemplateRequest request) {
+    private NotificationTemplateResponse prepareSMSTemplate(NotificationTemplateRequest request) {
+        NotificationTemplateResponse response = new NotificationTemplateResponse();
+        String smsTemplateString = "";
+        if ("ViewBalance".equalsIgnoreCase(request.getNotificationTemplateName())) {
+            smsTemplateString = this.getBalanceSMSTemplate();
+        } else if ("PhoneNumberChanged".equalsIgnoreCase(request.getNotificationTemplateName())) {
+            smsTemplateString = this.getPhoneNumberChanged();
+        }
+        StringSubstitutor sub = new StringSubstitutor(prepareNotificationParamsMap(request));
+        String notificationContent = sub.replace(smsTemplateString);
+        response.setSmsContent(notificationContent);
+        return response;
+    }
+
+    private NotificationTemplateResponse prepareEmailTemplate(NotificationTemplateRequest request) {
+        String notificationContent = prepareNotificationContent(request);
+        NotificationTemplateResponse response = new NotificationTemplateResponse();
+        response.setEmailContent(notificationContent);
+        if ("ViewBalance".equalsIgnoreCase(request.getNotificationTemplateName())) {
+            response.setEmailSubject("Your Bank Balance");
+        } else if ("PhoneNumberChanged".equalsIgnoreCase(request.getNotificationTemplateName())) {
+            response.setEmailSubject("Your Phone Number Changed");
+        }
+        return response;
+    }
+
+    private String prepareNotificationContent(NotificationTemplateRequest request) {
+        String notificationContent = "";
+        Context context = new Context();
+        Map<String, Object> notificationParametersMap = prepareNotificationParamsMap(request);
+        context.setVariables(notificationParametersMap);
+        File emailTemplateFile = new File("./src/main/resources/templates/" + request.getNotificationTemplateName() + ".html");
+        if (emailTemplateFile.exists()) {
+            notificationContent = templateEngine.process(request.getNotificationTemplateName() + ".html", context);
+        }
+        return notificationContent;
+    }
+
+    private Map<String, Object> prepareNotificationParamsMap(NotificationTemplateRequest request) {
         Map<String, Object> notificationParametersMap =
                 request.getNotificationParameters().
                         stream()
                         .collect(Collectors
                                 .toMap(NotificationParameter::getNotificationParameterName, NotificationParameter::getNotificationParameterValue));
-        NotificationTemplateResponse notificationTemplateResponse = new NotificationTemplateResponse();
-        String notificationContent = null;
+        return notificationParametersMap;
+    }
 
-        if ("EMAIL".equals(request.getNotificationMode())) {
-            Context context = new Context();
-            context.setVariables(notificationParametersMap);
-            File emailTemplateFile = new File("./src/main/resources/templates/" + request.getNotificationTemplateName() + ".html");
-            NotificationTemplateResponse response = new NotificationTemplateResponse();
-            if (emailTemplateFile.exists()) {
-                notificationContent = templateEngine.process("/PhoneNumberChanged.html", context);
-                notificationTemplateResponse.setEmailContent(notificationContent);
-                if("ViewBalance".equalsIgnoreCase(emailTemplateFile.getName())) {
-                    notificationTemplateResponse.setEmailSubject("Your Bank Balance");
-                } else if("PhoneNumberChanged".equalsIgnoreCase(emailTemplateFile.getName())) {
-                    notificationTemplateResponse.setEmailSubject("Your Phone Number Changed");
-                }
-            }
-        }
+    public static String getBalanceSMSTemplate() {
+        return "Hello ${name}"
+                .concat("\n")
+                .concat("Welcome to the Citizen Bank\n")
+                .concat("Your balance is ${balance}\n")
+                .concat("Thanks");
+    }
 
-        return notificationTemplateResponse;
+    private String getPhoneNumberChanged() {
+        return "Hello ${name}"
+                .concat("\n")
+                .concat("Welcome to the Citizen Bank\n")
+                .concat("Your Phonenumber is changed from ${oldPhoneNumber} to ${newPhoneNumber}\n");
     }
 }
