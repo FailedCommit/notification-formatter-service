9f68fd6f661ae483702043a999531d2d051ed486
Mohit Kumar <mkumar@tekion.com>
2021-07-17 07:35:27 +0530

    Initial commit

---
+++ src/main/java/com/manning/notification/formatter/NotificationFormatterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.manning.notification.formatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationFormatterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationFormatterServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/manning/notification/formatter/NotificationFormatterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manning.notification.formatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationFormatterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
